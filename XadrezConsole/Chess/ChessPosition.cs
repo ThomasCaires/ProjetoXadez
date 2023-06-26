2787da586fcf293f64523a272646464e19cd2fda
Thomas Caires <thomas_cairesaraujo@outlook.com>
2023-06-26 15:09:40 -0300

    Criada classe ChessPosition

---
+++ XadrezConsole/Chess/ChessPosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace Chess
+{
+    internal class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position toPosition()//metodo para convertar as posiçoes para a matriz
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -6,20 +6,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        try
-        {
-            Table tab = new Table (8, 8);
-
-            tab.ColocarPeca(new Rook(tab, Color.Black), new Position(9, 0));
-            tab.ColocarPeca(new Rook(tab, Color.Black), new Position(3, 3));
-            tab.ColocarPeca(new King(tab, Color.Black), new Position(3, 3));
-
-            Screen.ImprimirTabuleiro(tab);
-            Console.ReadLine();
-        }
-        catch (TableException e) {
-            Console.WriteLine(e.Message);
-        }
-
+        ChessPosition pos = new ChessPosition('b', 8);
+        Console.WriteLine(pos);
+        Console.WriteLine(pos.toPosition());
     }
 }
